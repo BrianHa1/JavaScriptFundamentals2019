db3b126e952ae042650a493b6e600dc26acf9c1d
Brian Ha <brianha32@gmail.com>
2019-12-09 20:11:13 -0500

    Another commit before doing 'git pull upstream master' again.

--- exercises/12-Module-Pattern/Module-Pattern.js
+++ exercises/12-Module-Pattern/Module-Pattern.js
@@ -21,6 +21,9 @@
 const { studentGrades } = require("../12-Module-Pattern/students");
 
 const TeacherGradeBook = (() => {
+  const studentAverage = () => {
+    studentGrades.grades.reduce((a, b) => a + b, 0) / studentGrades.grades.length;
+  };
   return {};
 })();
 
